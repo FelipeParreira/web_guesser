8bc93f322c8b6e0155c78587124fad41c97b2055
Felipe Parreira <felipeparreirafb@gmail.com>
2018-08-15 09:47:03 -0300

    I4.1: colorizing the output complete

--- web_guesser.rb
+++ web_guesser.rb
@@ -1,25 +1,47 @@
-require 'sinatra'
-require 'sinatra/reloader'
+require 'sinatra' 
+require 'sinatra/reloader' # to not need to reload the server when the file is changed
 
-SECRET_NUMBER = rand(100)
-set :number, SECRET_NUMBER
+# select random number between 0 and 100 (exclusive)
+SECRET_NUMBER = rand(100) 
+
+# Sinatra's settings feature
+set :number, SECRET_NUMBER 
+
+# define color constants
+BRIGHT_RED = "#ff0000"
+LIGHT_RED = "#fc8a8a"
+GREEN = "#00ff00"
 
 def check_guess(guess)
-	return '' if guess.to_s.empty?
+	# return an empty string if the user has not provided any input
+	return '' if guess.to_s.empty? 
+
 	guess = guess.to_i
-	appendix = "Way t"
+
+	# the values if the guess is right
+	message = "The SECRET NUMBER is #{settings.number}"
+	background_color = GREEN
+
 	if guess > settings.number
 		message = "Too high!"
-		message = appendix + message[1..-1] if guess > settings.number + 5
+		background_color = LIGHT_RED
 	elsif guess < settings.number
 		message = "Too low!"
-		message = appendix + message[1..-1] if guess < settings.number - 5
+		background_color = LIGHT_RED
 	end
-	return message
+
+	if (guess - settings.number).abs > 5
+		message = "Way t" + message[1..-1] 
+		background_color = BRIGHT_RED
+	end
+
+	return [message, background_color]
 end
 
 get '/' do
 	guess = params["guess"]
-	message = check_guess(guess)
-	erb :index, :locals => {:message => message}
+	result = check_guess(guess)
+	message = result[0]
+	background_color = result[1]
+	erb :index, :locals => {:message => message, :background_color => background_color}
 end
